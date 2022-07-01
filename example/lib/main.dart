ed86fd61eea1e3eded0f8f55fe2f116a8d6f9d92
chooyan-eng <chooyan.engineer@gmail.com>
2022-07-01 18:38:16 +0900

    Book の内容に合わせて修正

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,82 +11,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calendar Sample',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CalendarPage(),
+      home: const _CalendarSample(),
     );
   }
 }
 
-class CalendarPage extends StatefulWidget {
-  const CalendarPage({Key? key}) : super(key: key);
-
-  @override
-  State<CalendarPage> createState() => _CalendarPageState();
-}
-
-class _CalendarPageState extends State<CalendarPage> {
-  late DateTime _currentDate;
-
-  @override
-  void initState() {
-    _currentDate = DateTime.now();
-    super.initState();
-  }
-
-  void _toNextMonth() {
-    setState(() {
-      _currentDate = DateTime(_currentDate.year, _currentDate.month + 1);
-    });
-  }
-
-  void _toPreviousMonth() {
-    setState(() {
-      _currentDate = DateTime(_currentDate.year, _currentDate.month - 1);
-    });
-  }
+class _CalendarSample extends StatelessWidget {
+  const _CalendarSample({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calendar'),
-      ),
-      body: SizedBox.expand(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  InkWell(
-                    onTap: _toPreviousMonth,
-                    child: const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Icon(Icons.navigate_before),
-                    ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child:
-                          Text('${_currentDate.year}年 ${_currentDate.month}月'),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: _toNextMonth,
-                    child: const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Icon(Icons.navigate_next),
-                    ),
-                  ),
-                ],
-              ),
-              Calendar(date: _currentDate),
-            ],
-          ),
-        ),
+      appBar: AppBar(title: const Text('CalendarSample')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Calendar(date: DateTime(2022, 6)),
       ),
     );
   }
--- lib/calendar_widget.dart
+++ lib/calendar_widget.dart
@@ -14,12 +14,12 @@ class Calendar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final calendarData = CalendarBuilder().build(date);
-
     return Column(
       children: [
         const _WeekRow(['月', '火', '水', '木', '金', '土', '日']),
         ...calendarData.map(
           (week) => _WeekRow(
+            // 日付を文字列に変換。前後の月に所属する日付は null のため、その場合は空文字。
             week.map((date) => date?.toString() ?? '').toList(),
           ),
         ),
@@ -29,10 +29,7 @@ class Calendar extends StatelessWidget {
 }
 
 class _WeekRow extends StatelessWidget {
-  const _WeekRow(
-    this.datesOfWeek, {
-    super.key,
-  });
+  const _WeekRow(this.datesOfWeek);
 
   final List<String> datesOfWeek;
 
@@ -44,7 +41,7 @@ class _WeekRow extends StatelessWidget {
         (index) => Expanded(
           child: _DateBox(
             datesOfWeek[index],
-            weekday: index + 1,
+            weekday: index + 1, // 曜日は 1 始まりなので 1 を足す
           ),
         ),
       ).toList(),
@@ -56,24 +53,26 @@ class _DateBox extends StatelessWidget {
   const _DateBox(
     this.label, {
     required this.weekday,
-    super.key,
   });
 
+  /// 表示文字列（日付の他、「月」「火」といった曜日の場合もあり）
   final String label;
+
+  /// 曜日（土曜日は青、日曜日は赤く塗りつぶす）
   final int weekday;
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 1,
+      aspectRatio: 1, // 正方形にする
       child: Container(
         decoration: BoxDecoration(
           border: Border.all(color: Colors.black12, width: 1),
-          color: weekday == 6
+          color: weekday == 6 // 土曜日は青
               ? Colors.blue.shade50
-              : weekday == 7
+              : weekday == 7 // 日曜日は赤
                   ? Colors.red.shade50
-                  : Colors.white,
+                  : Colors.white, // その他は白
         ),
         child: Center(
           child: Text(label),
